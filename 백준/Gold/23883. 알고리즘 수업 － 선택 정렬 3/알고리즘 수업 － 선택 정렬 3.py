7b08369ae4b8ae2ea69ae62f737b70588c98c86f
Paik JongHwa <60594585+qor0530@users.noreply.github.com>
2023-07-27 04:55:20 +0900

    [Gold IV] Title: 알고리즘 수업 - 선택 정렬 3, Time: 596 ms, Memory: 227748 KB -BaekjoonHub

--- 백준/Gold/23883. 알고리즘 수업 － 선택 정렬 3/알고리즘 수업 － 선택 정렬 3.py
+++ 백준/Gold/23883. 알고리즘 수업 － 선택 정렬 3/알고리즘 수업 － 선택 정렬 3.py
@@ -9,7 +9,9 @@ for i, l in enumerate(Alist):
 for i in range(n - 1, -1, -1): #n-1 ~ 0
     if sorted_list[i] != Alist[i]:
         temp = [Alist[i], sorted_list[i]]
+        # 정렬된 리스트의 i번째 값과 Alist[i]의 값을 바꾼다.
         Alist[i], Alist[Adict[sorted_list[i]]] = Alist[Adict[sorted_list[i]]], Alist[i]
+        # dict의 value끼리 바꾼다.
         Adict[temp[0]], Adict[temp[1]] = Adict[temp[1]], Adict[temp[0]]
         k -= 1
         if k == 0:
