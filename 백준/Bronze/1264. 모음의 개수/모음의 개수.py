1d16e05b5625283862539d38eea0e72efaafbdfe
Paik JongHwa <60594585+qor0530@users.noreply.github.com>
2023-09-14 23:52:55 +0900

    [Bronze IV] Title: 모음의 개수, Time: 124 ms, Memory: 114328 KB -BaekjoonHub

---
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.py
@@ -0,0 +1,8 @@
+string = input()
+while string != "#":
+    count = 0
+    for i in string:
+        if i == "a" or i == "e" or i == "i" or i == "o" or i == "u" or i == "A" or i == "E" or i == "I" or i == "O" or i == "U":
+            count += 1
+    print(count)
+    string = input()
