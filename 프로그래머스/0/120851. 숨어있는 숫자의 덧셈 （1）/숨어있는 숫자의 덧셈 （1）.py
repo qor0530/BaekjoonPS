d821054c1a417612cece143680fee2c4d983f6d0
JongHwa Paik <60594585+qor0530@users.noreply.github.com>
2025-03-26 17:40:06 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.12 ms, Memory: 9.33 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
@@ -0,0 +1,8 @@
+def solution(my_string):
+    answer = 0
+    for i in range(len(my_string)):
+        try:
+            answer += int(my_string[i])
+        except:
+            pass
+    return answer
