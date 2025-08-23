8cc8b854db95db1fedf00d5f7a2e70d942a3d670
JongHwa Paik <60594585+qor0530@users.noreply.github.com>
2025-08-23 15:56:38 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,14 @@
+a = input()
+
+if a == "M":
+    print("MatKor")
+elif a == "W":
+    print("WiCys")
+elif a == "C":
+    print("CyKor")
+elif a == "A":
+    print("AlKor")
+elif a == "$":
+    print("$clear")
+else:
+    pass
