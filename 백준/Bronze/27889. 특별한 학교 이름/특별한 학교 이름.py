b33688dad443a64df92f154c6b6046677155c3b4
JongHwa Paik <60594585+qor0530@users.noreply.github.com>
2025-12-20 13:45:01 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,8 @@
+uni = dict()
+
+uni["NLCS"] =  "North London Collegiate School"
+uni["BHA"]  = "Branksome Hall Asia"
+uni["KIS"]  = "Korea International School"
+uni["SJA"]  = "St. Johnsbury Academy"
+
+print(uni[input()])
